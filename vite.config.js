e372f6ad2d7a48a89967b311239ef98237d36f65
Idoga Emmanuel <siremms300@gmail.com>
2024-10-17 04:18:29 -0700

    made modifications

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,48 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 5173, // Vite's default port
+    proxy: {
+      '/api/v1': {
+        target: 'http://localhost:3000', // Your backend server
+        changeOrigin: true,
+        
+      }
+    }
+  }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 // export default defineConfig({
 //   plugins: [react()],
 // })
 
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true, // Allow access over the network
-    port: 5173 // Default Vite port
-  }
-})
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     host: true, // Allow access over the network
+//     port: 5173 // Default Vite port
+//   }
+// })
